7265f90ec6aab8ef10c02c06c8b32fb97c733bff
Ravi Kumar Karthikeyan <Ravi Kumar Karthikeyan>
2020-05-14 22:46:55 +0530

    refs initial commit.

---
+++ src/main/java/org/tama/todo/TaMa/TaMaApplication.java
@@ -0,0 +1,13 @@
+package org.tama.todo.TaMa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaMaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaMaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tama/todo/TaMa/TaMaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tama.todo.TaMa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaMaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
